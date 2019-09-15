9dd2cf6609e52469d0e2592af55d997b0e421798
github username <github email address>
2019-09-16 00:28:03 +0100

    added

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
 
-  @@items = ["Apples","Carrots","Pears"]
-  @@cart = []
+  @@items = ["Apples","Carrots","Pears","Oranges"]
+  @@cart = ["Apples"]
 
   def call(env)
     resp = Rack::Response.new
@@ -43,3 +43,4 @@ class Application
     end
   end
 end
+
